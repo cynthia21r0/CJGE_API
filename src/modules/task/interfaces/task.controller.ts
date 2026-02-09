a40027867bf487b33dd27d346c3fab2699280c16
cynthia21r0 <116527805+cynthia21r0@users.noreply.github.com>
2026-02-09 17:46:55 -0600

    Configuración de CRUD para tareas

--- src/modules/task/interfaces/task.controller.ts
+++ src/modules/task/interfaces/task.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 import { TaskService } from './task.service';
 
 @Controller('/api/task')
 export class TaskController {
   constructor(private taskSvc: TaskService) {}
 
+  //! http:localhost:3000/api/task
   @Get()
-  public getTasks(): string {
-    return this.taskSvc.getTasks();
+  public getAllTasks(): string {
+    return this.taskSvc.getAllTasks();
+  }
+
+  //! http:localhost:3000/api/task/1
+  @Get(':id')
+  public listTasksById(@Param('id') id: string) {
+    return this.taskSvc.getTasksById(parseInt(id));
+  }
+
+  @Post()
+  public insertTask(task: any): any {
+    this.taskSvc.insertTask(task);
+  }
+
+  @Put(':id')
+  public updateTask(id: number, task: any): any {
+    this.taskSvc.updateTasks(task);
+  }
+
+  @Delete(':id')
+  public deleteTask(id: number) {
+    this.taskSvc.deleteTasks(id);
   }
 }
--- src/modules/task/interfaces/task.service.ts
+++ src/modules/task/interfaces/task.service.ts
@@ -2,7 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TaskService {
-  public getTasks(): string {
-    return 'Listado de Tareas';
+  public getAllTasks(): string {
+    return 'Obteniendo todas las tareas';
+  }
+
+  public getTasksById(id: number): string {
+    return `Obteniendo tarea con ID: ${id}`;
+  }
+
+  public insertTask(task: any): any {
+    return task;
+  }
+
+  public updateTasks(task: any): any {
+    return task;
+  }
+
+  public deleteTasks(task: any): any {
+    return task;
   }
 }
